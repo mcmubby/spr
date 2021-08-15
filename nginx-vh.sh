950fa9a19a22c6da899366586aa02c05f6b2f688
Mubarak <12859241+mcmubby@users.noreply.github.com>
2021-08-15 09:30:17 +0100

    Update nginx-vh.sh

--- nginx-vh.sh
+++ nginx-vh.sh
@@ -3,7 +3,7 @@ sudo apt install -y nginx
 cd /var/www
 sudo mkdir secdom
 cd secdom
-sudo cp /dwl/spr-main/secdom.html .
+sudo cp /spr-main/secdom.html .
 cd /etc/nginx/sites-enabled
-sudo cp /dwl/spr-main/secdom .
+sudo cp /spr-main/secdom .
 sudo service nginx restart
