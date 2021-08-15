2a0a3aef8ad383b9e328cf916e6d7733ea011390
Mubarak <12859241+mcmubby@users.noreply.github.com>
2021-08-15 12:12:18 +0100

    Update apache-vh.sh

--- apache-vh.sh
+++ apache-vh.sh
@@ -3,7 +3,7 @@ sudo apt install -y apache2
 cd /var/www
 sudo mkdir secdom
 cd secdom
-sudo cp /spr-main/secdom.html .
+sudo cp /spr-main/secdom.html ./index.html
 cd /etc/apache2/sites-enabled
-sudo cp /spr-main/secdom.conf ./000-default.conf
+sudo cat /spr-main/secdom.conf | sudo tee -a ./000-default.conf
 sudo service apache2 reload
